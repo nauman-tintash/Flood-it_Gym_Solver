b2b5d913e50f3dd5e6b257dc521f5d7864bed128
Arooj-tintash <arooj.shahid@tintash.com>
2018-08-03 12:23:52 +0500

    Add a simple loop in the file

--- FloodItMain.py
+++ FloodItMain.py
@@ -1,8 +1,36 @@
-
-
+import gym_flood
+import gym
+from random import choice
+from collections import defaultdict
 
 #main function where the execusion should start
 def main():
     print ('Hello, let\'s Flood It!!!')
+    env = gym.make("Flood-v0")
+    observation, possible = env.reset()
+    env.render("ansi")
+    bestmoves = 22
+    
+    done = None
+    current = observation
+    previous = observation
+    stop = False
+
+    while stop != True:
+        for row in range(0,12):
+            for column in range(len(observation[row])):
+                current = observation[row][column]
+                previous = observation[row][column-1]
+                if (current != previous):
+                    observation, reward, done, info = env.step(current)
+                    env.render("ansi")
+                    print (len(info["moves"]))
+                if (len(info["moves"]) == bestmoves):
+                    print ("Failed to complete the game")
+                    stop = True
+                    observation, possible = env.reset()
+                    return
+            
+    
 
 main()
