477968b209db630be911e8d909d2c2c1439d94ce
nauman-tintash <nauman@tintash.com>
2018-08-03 12:15:13 +0500

    Sample main function to test the gym environment in graphics mode.

--- FloodItMain.py
+++ FloodItMain.py
@@ -1,7 +1,25 @@
 import gym_flood
+import gym
+
+from matplotlib import pyplot as plt
 
 #main function where the execusion should start
 def main():
     print ('Hello, let\'s Flood It!!!')
+
+    env = gym.make("Flood-v0")
+    observation, possible = env.reset()
+    print(observation)
+    env.render()
+
+    observation, reward, done, info = env.step(2)
+    print(observation)
+    env.render()
+
+    env.step(3)
+    print(observation)
+    env.render()
+
+    plt.show()
 
 main()
